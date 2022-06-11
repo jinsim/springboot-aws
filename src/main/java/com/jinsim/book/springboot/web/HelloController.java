299e8f6992629654bc76dbaf32d32b10a72862b3
jinsim <simsime@korea.ac.kr>
2022-06-11 14:58:34 +0900

    dto 추가

--- src/main/java/com/jinsim/book/springboot/web/HelloController.java
+++ src/main/java/com/jinsim/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jinsim.book.springboot.web;
 
+import com.jinsim.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,13 @@ public class HelloController {
     public String hello() {
         // 즉, /hello로 GET 요청이 오면, "hello"를 반환하는 컨트롤러이다.
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(
+            @RequestParam("name") String name,
+            // @RequestParam은 외부에서 API로 넘긴 파라미터를 가져와서 변수로 저장하는 애너테이션이다.
+            @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
