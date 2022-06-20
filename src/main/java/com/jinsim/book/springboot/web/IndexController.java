1c13ce5f681c7c234ffd816b0e44a6d82d2b75ec
jinsim <simsime@korea.ac.kr>
2022-06-21 01:17:20 +0900

    게시글 등록 화면 구성

--- src/main/java/com/jinsim/book/springboot/web/IndexController.java
+++ src/main/java/com/jinsim/book/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.jinsim.book.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor // final이 붙은 필드의 생성자 자동 생성
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
