4538f812d5edaac7e97a0ef749df04afa136d7d7
jinsim <simsime@korea.ac.kr>
2022-06-21 13:59:44 +0900

    게시글 조회 기능 추가

--- src/main/java/com/jinsim/book/springboot/service/PostsService.java
+++ src/main/java/com/jinsim/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.jinsim.book.springboot.service;
 
 import com.jinsim.book.springboot.domain.posts.Posts;
 import com.jinsim.book.springboot.domain.posts.PostsRepository;
+import com.jinsim.book.springboot.web.dto.PostsListResponseDto;
 import com.jinsim.book.springboot.web.dto.PostsResponseDto;
 import com.jinsim.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jinsim.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -38,5 +42,12 @@ public class PostsService {
                         IllegalArgumentException("해당 게시글이 없습ㄴ디ㅏ. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/jinsim/book/springboot/web/IndexController.java
+++ src/main/java/com/jinsim/book/springboot/web/IndexController.java
@@ -1,15 +1,20 @@
 package com.jinsim.book.springboot.web;
 
+import com.jinsim.book.springboot.domain.posts.PostsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor // final이 붙은 필드의 생성자 자동 생성
 @Controller
 public class IndexController {
 
+    private final PostsRepository postsRepository;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsRepository.findAllDesc());
         return "index";
     }
 
