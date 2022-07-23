a8fce0d28ac0e7b6b3ea698f9ad394dce7856f6b
jinsim <simsime@korea.ac.kr>
2022-07-24 00:04:22 +0900

    CustomOAuth2UserService 클래스 생성

--- src/main/java/com/jinsim/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/jinsim/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -1,15 +1,21 @@
 package com.jinsim.book.springboot.config.auth;
 
+import com.jinsim.book.springboot.config.auth.dto.OAuthAttributes;
+import com.jinsim.book.springboot.config.auth.dto.SessionUser;
+import com.jinsim.book.springboot.domain.user.User;
 import com.jinsim.book.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
@@ -31,6 +37,19 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                 .of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
         User user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user", new SessionUser(user));
 
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
     }
 }
